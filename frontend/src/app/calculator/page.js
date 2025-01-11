acb36ee883a4c54b5b26e2774adda18b79bfa8fb
Mihails Simvulidi <mihails.simvulidi@gmail.com>
2025-01-11 19:55:15 +0200

    feat(calculator): incorporated into frontend

--- frontend/src/app/calculator/page.js
+++ frontend/src/app/calculator/page.js
@@ -1,25 +1,9 @@
-import Page from "../../components/Page";
-import { buildMetadata } from "../../utils/seo";
-import { getGlobal, getPageBySlug, findSpecialPage } from "../../utils/strapi";
+import { getGlobal } from "../../utils/strapi";
 import Calculator from "./Calculator";
-
-// TODO non-page specific
-// export async function generateMetadata({ params }) {
-//   const slug = getSlug(params);
-//   const global = await getGlobal();
-//   const specialPage = await findSpecialPage(slug);
-
-//   if (specialPage) {
-//     return buildMetadata(global, specialPage.entity.metadata);
-//   }
-
-//   const page = await getPageBySlug(slug);
-
-//   return buildMetadata(global, page.metadata);
-// }
+import { fetchEvaluations } from "./utils/impact";
 
 export default async function CalculatorPage() {
-  const global = await getGlobal();
+  const evaluations = await fetchEvaluations();
 
-  return <Calculator global={global} />;
+  return <Calculator evaluations={evaluations} />;
 }
