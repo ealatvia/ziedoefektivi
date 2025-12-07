599c918104fbeca03edb13c5a524f98324a91b86
Akuukis <akuukis@kalvis.lv>
2025-12-07 14:09:43 +0200

    feat(navbar): close hamburger after click

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -82,6 +82,7 @@ export default function Navbar({ global }) {
                   key={link.id}
                   href={link.href}
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-slate-900 hover:bg-primary-100"
+                  onClick={() => setMobileMenuOpen(false)}
                 >
                   {link.text}
                 </Anchor>
